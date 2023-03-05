2afa54addc493b8a24dc03b30d7ac190f852a456
radarsada <bachhavpiyush68@gmail.com>
2023-03-05 23:03:28 +0530

    d2M3T23.03

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../Components/Logo";
 
+import Wrapper from "../assets/Wrappers/RegisterPage";
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 const Register = () => {
-  return <h1>Registration</h1>;
+  const [values, setvalues] = useState(initialState);
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-block"></button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
